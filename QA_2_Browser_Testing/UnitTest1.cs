4b7dbb8ea8cda5117c3bb3693017e7c6d34549ef
Kevin Le <kevinle108@gmail.com>
2023-03-01 10:19:26 -0500

    set up first test

--- QA_2_Browser_Testing/UnitTest1.cs
+++ QA_2_Browser_Testing/UnitTest1.cs
@@ -1,11 +1,18 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
 namespace QA_2_Browser_Testing
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        [Trait("Category", "Smoke")]
+        public void LoadPage()
         {
-
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("https://svelte.dev/");
+            }
         }
     }
 }

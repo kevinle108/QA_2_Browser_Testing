9eca3bb675c66d736750937141844bbe6d75f9e4
Kevin Le <kevinle108@gmail.com>
2023-03-01 16:22:45 -0500

    read ele text

--- QA_2_Browser_Testing/UnitTest1.cs
+++ QA_2_Browser_Testing/UnitTest1.cs
@@ -12,10 +12,15 @@ namespace QA_2_Browser_Testing
         {
             using (IWebDriver driver = new ChromeDriver())
             {
-                driver.Navigate().GoToUrl("https://svelte.dev/");
+                driver.Navigate().GoToUrl("https://www.redfin.com/KY/Louisville/8110-Kellerman-Rd-40219/home/71182885");
 
                 // QR Code Generator
                 // https://me-qr.com/
+                // https://www.qr-code-generator.com/
+                // Date Picker
+                // https://qrickit.com/qrickit_apps/qrickit_qrcode_creator_vcal.php
+                // https://www.qrcodechimp.com/qr-code-generator-for-event
+
 
                 // QR Code Readers
                 // https://qreader.online/
@@ -23,8 +28,10 @@ namespace QA_2_Browser_Testing
                 // https://4qrcode.com/scan-qr-code.php
 
                 string title = driver.Title;
+                IWebElement houseinfo = driver.FindElement(By.Id("house-info"));
 
-                title.Should().Be("Svelte • Cybernetically enhanced web apps");
+                title.Should().Be("8110 Kellerman Rd, Louisville, KY 40219 | MLS# 1631002 | Redfin");
+                houseinfo.Text.Should().Be("");
                 //title.Should().Be("Docs • Svelte");
 
             }

0236c541699918c681ccaf42b592644a0890c0df
Kevin Le <kevinle108@gmail.com>
2023-03-02 18:45:17 -0500

    add possible sites

--- QA_2_Browser_Testing/UnitTest1.cs
+++ QA_2_Browser_Testing/UnitTest1.cs
@@ -9,6 +9,54 @@ namespace QA_2_Browser_Testing
     public class UnitTest1
     {
         [Fact]
+        public void LoadPage_()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("https://www.redfin.com/KY/Louisville/8110-Kellerman-Rd-40219/home/71182885");
+
+
+                // https://me-qr.com/
+                // Date Picker
+                // https://qrickit.com/qrickit_apps/qrickit_qrcode_creator_vcal.php
+                // https://www.qrcodechimp.com/qr-code-generator-for-event
+                // QR Code Readers
+                // https://qreader.online/
+                // https://me-qr.com/
+                // https://4qrcode.com/scan-qr-code.php
+
+                // Generate, then Read
+                // https://4qrcode.com/ + https://4qrcode.com/scan-qr-code.php
+                // https://qreader.online/generate + https://qreader.online/
+                // https://me-qr.com/ + https://me-qr-scanner.com/qr-scanner#scan-using-file
+                List<String> names = new()
+                {
+                    "Kevin", "Le"
+                };
+
+                names.Should().StartWith(names);
+
+
+                // EXPLICIT WAIT EXAMPLE
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(1));
+                IWebElement applyLink =
+                    wait.Until(d => d.FindElement(By.LinkText("Easy: Apply Now!")));
+
+
+                
+
+                string title = driver.Title;
+                IWebElement houseinfo = driver.FindElement(By.Id("house-info"));
+
+                title.Should().Be("8110 Kellerman Rd, Louisville, KY 40219 | MLS# 1631002 | Redfin");
+                houseinfo.Text.Should().Be("");
+                //title.Should().Be("Docs • Svelte");
+
+            }
+        }
+
+
+        [Fact]
         [Trait("Category", "Example")]
         public void LoadPage()
         {
@@ -51,6 +99,7 @@ namespace QA_2_Browser_Testing
             }
         }
 
+        // TODO: Remove this
         [Fact]
         [Trait("Category", "Example")]
         public void Example_Explicit_Wait()
@@ -73,21 +122,6 @@ namespace QA_2_Browser_Testing
             {
                 driver.Navigate().GoToUrl("https://www.redfin.com/KY/Louisville/8110-Kellerman-Rd-40219/home/71182885");                
                 
-                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(1));
-                IWebElement applyLink =
-                    wait.Until(ExpectedConditions.ElementToBeClickable(By.LinkText("Easy: Apply Now!")));
-                applyLink.Click();
-            }
-        }
-
-        [Fact]
-        [Trait("Category", "Example")]
-        public void Example_Prebuilt_Conditions()
-        {
-            using (IWebDriver driver = new ChromeDriver())
-            {
-                driver.Navigate().GoToUrl("https://www.redfin.com/KY/Louisville/8110-Kellerman-Rd-40219/home/71182885");
-
                 WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(1));
                 IWebElement applyLink =
                     wait.Until(ExpectedConditions.ElementToBeClickable(By.LinkText("Easy: Apply Now!")));
